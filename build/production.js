121f35dfb527c06bb41a3073a4d19a4e4d42ecfb
aaronallan <aaron.a.pollock@gmail.com>
2017-08-29 22:37:12 -0400

    style updates

--- build/production.js
+++ build/production.js
@@ -126,23 +126,6 @@ var ContentPaneView = Backbone.View.extend({
 	  	var html = $(template);
 	  	that.$el.html(html);
 	  	that.renderImage();
-
-	  	if (that.template === 'home') {
-				$('.fa-map-pin a').on('click', function () {
-					$('.modal').addClass('show');
-				});
-			}
-
-			if (that.template === 'about') {
-				$('.continue-reading').on('click', function () {
-					$('.history-modal').addClass('show');
-				});
-
-				for (var i = 2; i <= 6; i++) {
-					$('.secondary').hide();
-					$('.secondary').delay(2000 * i).fadeIn(500).delay(2000 * i).fadeOut(500);
-				};
-			}
 		});
 
 		return this;
